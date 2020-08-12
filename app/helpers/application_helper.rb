c20fbebc291580fcbfe87762f9072d64f55103dc
AssanSav <assane.savadogo81@gmail.com>
2020-08-12 19:13:19 -0400

    Recipe date strftime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+   def created_date_of(recipe)
+        recipe.created_at.strftime("%A, %b %e, at %l:%M %p")
+    end
+
+    def updated_date_of(recipe)
+        recipe.updated_at.strftime("%A, %b %e, at %l:%M %p")
+    end
+    
 end
