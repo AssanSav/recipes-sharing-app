f65adf2ab0d3559076d9e761373796cbf854d299
AssanSav <assane.savadogo81@gmail.com>
2020-08-12 19:06:42 -0400

    Routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   get 'ingredients/edit'
+    get 'ingredients/update'
+    root to: 'page#home'
+    devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
+
+    resources :ingredients
+    resources :comments
+    resources :recipes 
+
+    resources :categories do 
+        resources :recipes
+    end
+
+    resources :recipes do 
+        resources :comments
+    end
 end
