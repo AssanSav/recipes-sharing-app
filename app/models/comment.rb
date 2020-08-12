705ee4956a6c55e097a775e940ce1b47657d1728
AssanSav <assane.savadogo81@gmail.com>
2020-08-12 18:29:09 -0400

    Comment model created plus validations and relathionships types added

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ApplicationRecord
+    belongs_to :recipe
+    belongs_to :user
+
+    validates :content, presence: true, length: {minimum: 2}
+
+    def self.group_by_user
+        all.group_by(&:user)
+    end
+    
+end
