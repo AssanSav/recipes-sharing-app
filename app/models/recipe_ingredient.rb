e80a842d0753b5af614a13df12ce0bf6cd3ae697
AssanSav <assane.savadogo81@gmail.com>
2020-08-12 18:43:58 -0400

    Custom accept ingredients nested attributes

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -5,5 +5,11 @@ class RecipeIngredient < ApplicationRecord
     validates :amount, presence: true
     validates_uniqueness_of :ingredient_id, scope: :recipe_id
     
-  
+  def ingredient_attributes=(ingredients_attributes)
+      ingredients_attributes.values.each do |ingredient_name|
+          if ingredient_name.present? 
+              self.ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+          end
+      end
+  end
 end
