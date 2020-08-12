edb92bdaf6aa981f8d0d381666f8ab813c191a0a
AssanSav <assane.savadogo81@gmail.com>
2020-08-12 18:45:26 -0400

    validations and relathionships types added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+            :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+    has_many :recipes
+    has_many :comments
+
+    validates :username, presence: true, uniqueness: true, length: {minimum: 5}, on: :create
+    validates :first_name, :last_name, presence: true, length: { maximum: 30 }
 end
