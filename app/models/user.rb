b46f1dd59f82fd59d2f01d46ec9c6ce650a18f95
AssanSav <assane.savadogo81@gmail.com>
2020-08-12 18:46:19 -0400

    Facebook auth method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true, length: {minimum: 5}, on: :create
     validates :first_name, :last_name, presence: true, length: { maximum: 30 }
+
+    def self.from_omniauth(auth) 
+        user = where(provider: auth.provider, uid: auth.uid).first 
+        unless user 
+            user = where(email: auth.info.email).first_or_initialize 
+            user.email = auth.info.email
+            user.name = auth.info.name 
+            user.image = auth.info.image 
+            user.password = Devise.friendly_token[0,20]
+            (user.save!(validate: false))
+        end
+        user
+    end
 end
